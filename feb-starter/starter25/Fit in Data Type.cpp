d083c5f0158d4dbfa4c733d2edb7ac4c2e7d1ee9
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-02-11 23:18:03 +0530

    Create Fit in Data Type.cpp

---
+++ feb-starter/starter25/Fit in Data Type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x;
+	    cin>>n>>x;
+	    n = n+1;
+	    cout<<x%n<<endl;
+	    
+	}
+	
+	return 0;
+}
