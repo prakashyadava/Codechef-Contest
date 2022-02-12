42de56287d087bfea144572d93b46d0940bd4279
Prakash Yadava <79514008+prakashyadava@users.noreply.github.com>
2022-02-12 23:47:17 +0530

    Create CHEFBARBER.cpp

---
+++ feb-starter/CHEFBARBER.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,m;
+	    cin>>n>>m;
+	    cout<<n*m<<endl;
+	}
+	
+	return 0;
+}
